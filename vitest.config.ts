f269aadb9cf19cc2c10d005db177d2fa020cf499
victorlvl47 <victor.lvl47@gmail.com>
2026-07-14 20:00:37 -0600

    update vitest config with alias and remove globals

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,20 @@
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "react-native": fileURLToPath(
+        new URL("./test/react-native.ts", import.meta.url),
+      ),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   ssr: {
     noExternal: ["@testing-library/react-native"],
   },
   test: {
-    globals: true,
-    alias: [
-      {
-        find: /^react-native$/,
-        replacement: new URL("./test/react-native.ts", import.meta.url).pathname,
-      },
-    ],
+    environment: "node",
     deps: {
       optimizer: {
         ssr: {
